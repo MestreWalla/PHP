031ee04e537581fed113008090ab8ce6b56081f5
Maycon Vitor Correa <105678497+MestreWalla@users.noreply.github.com>
2023-08-16 19:36:07 -0300

    Create while.php

---
+++ Maycon PHP/16082023/while.php
@@ -0,0 +1,20 @@
+<?php
+
+// forma 01
+	$x = 4;
+	while ($x <= 5) {
+		echo "Numero: $x <br>";
+		$x++;
+	}
+	echo("<hr>");
+// forma 02
+	do {
+		echo "Numero: $x \n";
+		$x = $x +1;
+	} while ($x <= 5);
+	echo("<hr>");
+// forma 03
+	for ($i = 1; $i <= 10; $i++) {
+		echo $i . "<br>" . "\n"; // (\n para prompt) e (<br> para navegador)
+	}
+?>
