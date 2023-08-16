d47cbc0708de8989a2dfe73032e1a556a8bf3434
Maycon Vitor Correa <105678497+MestreWalla@users.noreply.github.com>
2023-08-16 20:29:48 -0300

    Update while.php

--- Maycon PHP/16082023/while.php
+++ Maycon PHP/16082023/while.php
@@ -15,6 +15,47 @@
 	echo("<hr>");
 // forma 03
 	for ($i = 1; $i <= 10; $i++) {
-		echo $i . "<br>" . "\n"; // (\n para prompt) e (<br> para navegador)
+		echo $i . "<br>" . "\n"; // pular linha com (\n para prompt) e (<br> para navegador)
 	}
+	echo("<hr>");
+// forma 04
+	$cores = array("verde,"." amarelo,"." azul,"." branco,");
+	foreach ($cores as $cor) {
+		echo "Cor: $cor <br>";
+	}
+	echo("<hr>");
+
+// Exercicio
+
+	//Receba quatro notas de um aluno e calcule a sua media, dizendo se ele foi:
+	//A - Aprovado - >=7
+	//B - Exame - média >=5
+	//C - Reprovado - média<5
+	$nota01 = 05;
+	$nota02 = 03;
+	$nota03 = 06;
+	$nota04 = 02;
+	$resultado = 0;
+
+	$media = $nota01+$nota02+$nota03+$nota04;
+	echo "Media = ".$media." - ";
+
+	if ($media >= 7) {
+		$resultado = 0;
+	} elseif ($media >= 5) {
+		$resultado = 1;
+	} else {
+		$resultado = 2;
+	}
+
+	switch ($resultado) {
+	case 0:
+		echo "Aprovado";
+		break;
+	case 1:
+		echo "Exame";
+		break;
+	default:
+	echo "Reprovado";
+}
 ?>
