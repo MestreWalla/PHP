a374aac8d7dc5f08c6204c29f3cfb33411afb441
Maycon Vitor Correa <105678497+MestreWalla@users.noreply.github.com>
2023-08-22 21:48:49 -0300

    update

--- Maycon PHP/22082023/Exercicio01/Form.php
+++ Maycon PHP/22082023/Exercicio01/Form.php
@@ -1,12 +1,43 @@
 <?php
 
-	include "Funcao.php";
-	$x = $_POST['num1'];
-	$y = $_POST['num2'];
+include "Funcao.php";
+$x = $_POST['num1'];
+$y = $_POST['num2'];
+$tipo = $_POST['tipo'];
+$c = "0";
 
-	echo "Resultado: $x + $y = ".soma($x,$y);
-	echo "<br>";
+if ($tipo == 'soma') {
+    $c = "+";
+} elseif ($tipo == 'subtracao') {
+    $c = "-";
+} elseif ($tipo == 'multiplicacao') {
+    $c = "*";
+} else {
+    $c = "/";
+}
 
-	echo "<style> body {background-color: black; color: white;} </style>";
+
+$resultado = "0";
+if ($c == "+") {
+    $resultado = soma($x, $y);
+} elseif ($c == "-") {
+    $resultado = subtracao($x, $y);
+} elseif ($c == "*") {
+    $resultado = multiplicacao($x, $y);
+} elseif ($c == "/") {
+    $resultado = divisao($x, $y);
+}
+
+echo "Resultado: $x $c $y = $resultado";
+echo "<br>";
+
+echo "<style>
+    body {
+        background-color: black;
+        color: white;
+        display: flex;
+        justify-content: center;
+    }
+</style>";
 
 ?>
--- Maycon PHP/22082023/Exercicio01/Funcao.php
+++ Maycon PHP/22082023/Exercicio01/Funcao.php
@@ -3,5 +3,18 @@
 	function Soma($a,$b){
 		return($a+$b);
 	}
+	function Subtracao($a,$b){
+		return($a-$b);
+	}
+	function Multiplicacao($a,$b){
+		return($a*$b);
+	}
+	function Divisao($a,$b){
+		if ($a != 0){
+			return ($a/$b);
+		} else {
+			return ("Não é possivel dividir por zero !!");	
+		}
+	}
 
 ?>
