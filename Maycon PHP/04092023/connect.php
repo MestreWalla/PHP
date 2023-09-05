ba51732a9a1e0be64461b44f9083d450fe61bac3
Maycon Vitor Correa <105678497+MestreWalla@users.noreply.github.com>
2023-09-04 21:55:29 -0300

    update

---
+++ Maycon PHP/04092023/connect.php
@@ -0,0 +1,10 @@
+<?php
+	//Dados de conexão com a base de dados
+	$servername = "localhost";
+	$username = "root";
+	$password = "";
+	$dbname = "alunos";
+
+	//Criar a conexao com o banco de dados
+	$conn = mysqli_connect($servername, $username, $password, $dbname);
+?>
