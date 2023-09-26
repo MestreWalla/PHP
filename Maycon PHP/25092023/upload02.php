d2f9de7ebcc1b80221eb11e5a5e59157503fbfff
Maycon Vitor Correa <105678497+MestreWalla@users.noreply.github.com>
2023-09-25 21:42:01 -0300

    update

---
+++ Maycon PHP/25092023/upload02.php
@@ -0,0 +1,24 @@
+<?php
+
+if (isset($_POST['enviar-formulario'])):
+    $formatos = array("png", "jpg", "jpeg", "gif", "pdf");
+    $extensao = pathinfo($_FILES['arquivo']['name'], PATHINFO_EXTENSION);
+
+    if(in_array($extensao, $formatos)):
+        //echo "Existe";
+        $pasta = "arquivos/";
+        $temporario = $_FILES['arquivo']['tpm_name'];
+        $novoNome = uniqid().".$extensao";
+
+        if(move_uploaded_file($temporario, $pasta.$novoNome)):
+            $mensagem = "Upload feito com sucesso.";
+        else:
+            $mensagem = "Erro, não foi possivel fazer o upload.";
+        endif;
+    else:
+        //echo "Não existe";
+        $mensagem = "Formato inválido!!";
+    endif;
+    echo $mensagem;
+endif;
+echo "<br> <a href='index.php'> << voltar </a>";
