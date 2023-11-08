85762ee1fd9c829c366665b23f464d81c71cd1f9
Maycon Vitor Correa <105678497+MestreWalla@users.noreply.github.com>
2023-11-07 21:02:02 -0300

    update

---
+++ Maycon PHP/07112023/siteTeste/login.php
@@ -0,0 +1,112 @@
+<?php
+session_start(); // Iniciar a sessão (se já não estiver iniciada)
+
+define('HOST', '127.0.0.1');
+define('USUARIO', 'root');
+define('SENHA', '');
+define('DB', 'aluno');
+
+$conexao = mysqli_connect(HOST, USUARIO, SENHA, DB) or die ('Não foi possível conectar');
+
+if ($_SERVER['REQUEST_METHOD'] == 'POST') {
+    $usuario = $_POST['usuario'];
+    $senha = $_POST['senha'];
+
+    $query = "SELECT * FROM login WHERE user = '$usuario' AND pass = MD5('$senha')";
+    $resultado = mysqli_query($conexao, $query);
+
+    if (mysqli_num_rows($resultado) == 1) {
+        // As credenciais são válidas
+        $_SESSION['usuario'] = $usuario;
+        header('Location: dashboard.php'); // Redirecionar para a página de painel
+        exit();
+    } else {
+        $erro = 'Credenciais inválidas. Tente novamente.';
+    }
+}
+
+?>
+
+<!DOCTYPE html>
+<html>
+<head>
+    <title>Página de Login</title>
+    <style>
+        body {
+            background-color: #f5f5f5;
+            font-family: Arial, sans-serif;
+            margin: 0;
+            display: flex;
+            justify-content: center;
+            align-items: center;
+            height: 100vh;
+        }
+
+        .login-container {
+            background-color: #fff;
+            border-radius: 5px;
+            box-shadow: 0 0 10px rgba(0, 0, 0, 0.1);
+            padding: 20px;
+            width: 300px;
+        }
+
+        h2 {
+            text-align: center;
+        }
+
+        form {
+            display: flex;
+            flex-direction: column;
+        }
+
+        label {
+            font-weight: bold;
+            margin-bottom: 5px;
+        }
+
+        input[type="text"],
+        input[type="password"] {
+            padding: 10px;
+            border: 1px solid #ccc;
+            border-radius: 5px;
+            margin-bottom: 10px;
+        }
+
+        input[type="submit"] {
+            background-color: #007bff;
+            color: #fff;
+            padding: 10px;
+            border: none;
+            border-radius: 5px;
+            cursor: pointer;
+            transition: background-color 0.3s;
+        }
+
+        input[type="submit"]:hover {
+            background-color: #0056b3;
+        }
+
+        p.error-message {
+            color: #ff0000;
+            text-align: center;
+        }
+    </style>
+</head>
+<body>
+    <div class="login-container">
+        <h2>Login</h2>
+        <?php if (isset($erro)) { echo '<p class="error-message">' . $erro . '</p>'; } ?>
+        <form method="post">
+            <label for="usuario">Usuário:</label>
+            <input type="text" id="usuario" name="usuario" required>
+
+            <label for="senha">Senha:</label>
+            <input type="password" id="senha" name="senha" required>
+
+            <input type="submit" value="Login">
+            <a href="cadastro.php"><input type="button" value="cadastro"></a>
+        </form>
+    </div>
+</body>
+</html>
+
