5e9996135cf30ef562131dc67ab051a30f766da7
Maycon Vitor Correa <105678497+MestreWalla@users.noreply.github.com>
2023-11-23 21:59:40 -0300

    update Maycon PHP/23112023/exercicio.php

--- Maycon PHP/23112023/exercicio.php
+++ Maycon PHP/23112023/exercicio.php
@@ -1,4 +1,17 @@
 Exercicio
 Crie uma classe correspondente a uma conta de banco que tera as seguintes caracteristicas:
 -Atributos: nome, numero da conta, saldo.
--Metodos:
+-Metodos: abrir conta, sacar, depositar, consultar `saldo, consultar
+dados do cliente.
+Saldo inicial será de R$100,00.
+
+Crie uma nova classe e exiba o menu com as seguintes operações:
+1 - Abrir conta.
+2 - Consultar Saldo.
+3 - Sacar.
+4 - Depositar
+5 - Consultar dados do cliente.
+6 - Sair.
+
+Execute o programa e teste as operações bancárias do menu informando
+os dados necessários para cada opção. Exibir o menu ao final de cada operação.
