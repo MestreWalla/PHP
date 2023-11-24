353ece024aa568e4594cae790204ee5783545996
Maycon Vitor Correa <105678497+MestreWalla@users.noreply.github.com>
2023-11-23 22:49:04 -0300

    update Maycon PHP/23112023/exercicio.php

--- Maycon PHP/23112023/exercicio.php
+++ Maycon PHP/23112023/exercicio.php
@@ -15,27 +15,94 @@ Crie uma nova classe e exiba o menu com as seguintes operações:
 
 Execute o programa e teste as operações bancárias do menu informando
 os dados necessários para cada opção. Exibir o menu ao final de cada operação.
+
 <?php
-class cliente
+
+class Cliente
 {
     private $nome;
     private $sobrenome;
     private $cpf;
     private $idade;
     private $endereco;
-}
-public function __construct($nome, $sobrenome, $cpf, $idade, $endereco){
-    $this->setNome($nome);
-    $this->setSobreNome($sobreBome);
-    $this->setCpf($cpf);
-    $this->setIdade($idade);
-    $this->setEndereco($endereco);
-}
-class conta
-{
-    private $nConta;
+    private $numeroConta;
     private $saldo;
-    private $poupanca;
-    private $limite;
-    private $transacoes;
+
+    public function __construct($nome, $sobrenome, $cpf, $idade, $endereco, $numeroConta)
+    {
+        $this->nome = $nome;
+        $this->sobrenome = $sobrenome;
+        $this->cpf = $cpf;
+        $this->idade = $idade;
+        $this->endereco = $endereco;
+        $this->numeroConta = $numeroConta;
+        $this->saldo = 100.00; // Saldo inicial de R$100,00
+    }
+
+    public function abrirConta()
+    {
+        // Pedir variáveis para abrir conta
+        echo "Informe os dados para abrir a conta:\n";
+        $this->nome = readline("Nome: ");
+        $this->sobrenome = readline("Sobrenome: ");
+        $this->cpf = readline("CPF: ");
+        $this->idade = readline("Idade: ");
+        $this->endereco = readline("Endereço: ");
+        $this->numeroConta = readline("Número da Conta: ");
+
+        // Lógica para abrir conta com verificação de idade
+        if ($this->idade >= 18) {
+            echo "Conta aberta com sucesso.\n";
+        } else {
+            echo "Cliente não atende à idade mínima para abrir uma conta.\n";
+        }
+    }
+
+    public function sacar($valor)
+    {
+        // Lógica para sacar
+        if ($valor > 0 && $valor <= $this->saldo) {
+            $this->saldo -= $valor;
+            echo "Saque de R$ " . $valor . " realizado com sucesso.\n";
+        } else {
+            echo "Saldo insuficiente ou valor inválido para saque.\n";
+        }
+    }
+
+    public function depositar($valor)
+    {
+        // Lógica para depositar
+        if ($valor > 0) {
+            $this->saldo += $valor;
+            echo "Depósito de R$ " . $valor . " realizado com sucesso.\n";
+        } else {
+            echo "Valor inválido para depósito.\n";
+        }
+    }
+
+    public function consultarSaldo()
+    {
+        // Lógica para consultar saldo
+        echo "Saldo atual: R$ " . $this->saldo . "\n";
+    }
+
+    public function consultarDadosCliente()
+    {
+        // Lógica para consultar dados do cliente
+        echo "Nome: " . $this->nome . "\n";
+        echo "Sobrenome: " . $this->sobrenome . "\n";
+        echo "CPF: " . $this->cpf . "\n";
+        echo "Idade: " . $this->idade . "\n";
+        echo "Endereço: " . $this->endereco . "\n";
+        echo "Número da Conta: " . $this->numeroConta . "\n";
+        echo "Saldo: R$ " . $this->saldo . "\n";
+    }
 }
+
+// Exemplo de uso
+$cliente = new Cliente("", "", "", "", "", "");
+$cliente->abrirConta();
+$cliente->consultarDadosCliente();
+
+// Implementação da classe de menu e execução do programa não foram fornecidas no código original.
+// Você pode adicionar essa lógica conforme necessário para atender aos requisitos do menu.
