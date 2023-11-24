26294c7002756bce25ad0346735c9cda07da2c5d
Maycon Vitor Correa <105678497+MestreWalla@users.noreply.github.com>
2023-11-24 12:45:21 -0300

    Update exercicio.php

--- Maycon PHP/23112023/exercicio.php
+++ Maycon PHP/23112023/exercicio.php
@@ -18,8 +18,7 @@ os dados necessários para cada opção. Exibir o menu ao final de cada operaç�
 
 <?php
 
-class Cliente
-{
+class Cliente {
     private $nome;
     private $sobrenome;
     private $cpf;
@@ -28,8 +27,7 @@ class Cliente
     private $numeroConta;
     private $saldo;
 
-    public function __construct($nome, $sobrenome, $cpf, $idade, $endereco, $numeroConta)
-    {
+    public function __construct($nome, $sobrenome, $cpf, $idade, $endereco, $numeroConta) {
         $this->nome = $nome;
         $this->sobrenome = $sobrenome;
         $this->cpf = $cpf;
@@ -39,8 +37,7 @@ class Cliente
         $this->saldo = 100.00; // Saldo inicial de R$100,00
     }
 
-    public function abrirConta()
-    {
+    public function abrirConta() {
         // Pedir variáveis para abrir conta
         echo "Informe os dados para abrir a conta:\n";
         $this->nome = readline("Nome: ");
@@ -51,51 +48,47 @@ class Cliente
         $this->numeroConta = readline("Número da Conta: ");
 
         // Lógica para abrir conta com verificação de idade
-        if ($this->idade >= 18) {
+        if($this->idade >= 18) {
             echo "Conta aberta com sucesso.\n";
         } else {
             echo "Cliente não atende à idade mínima para abrir uma conta.\n";
         }
     }
 
-    public function sacar($valor)
-    {
+    public function sacar($valor) {
         // Lógica para sacar
-        if ($valor > 0 && $valor <= $this->saldo) {
+        if($valor > 0 && $valor <= $this->saldo) {
             $this->saldo -= $valor;
-            echo "Saque de R$ " . $valor . " realizado com sucesso.\n";
+            echo "Saque de R$ ".$valor." realizado com sucesso.\n";
         } else {
             echo "Saldo insuficiente ou valor inválido para saque.\n";
         }
     }
 
-    public function depositar($valor)
-    {
+    public function depositar($valor) {
         // Lógica para depositar
-        if ($valor > 0) {
+        if($valor > 0) {
             $this->saldo += $valor;
-            echo "Depósito de R$ " . $valor . " realizado com sucesso.\n";
+            echo "Depósito de R$ ".$valor." realizado com sucesso.\n";
         } else {
             echo "Valor inválido para depósito.\n";
         }
     }
 
-    public function consultarSaldo()
-    {
+    public function consultarSaldo() {
         // Lógica para consultar saldo
-        echo "Saldo atual: R$ " . $this->saldo . "\n";
+        echo "Saldo atual: R$ ".$this->saldo."\n";
     }
 
-    public function consultarDadosCliente()
-    {
+    public function consultarDadosCliente() {
         // Lógica para consultar dados do cliente
-        echo "Nome: " . $this->nome . "\n";
-        echo "Sobrenome: " . $this->sobrenome . "\n";
-        echo "CPF: " . $this->cpf . "\n";
-        echo "Idade: " . $this->idade . "\n";
-        echo "Endereço: " . $this->endereco . "\n";
-        echo "Número da Conta: " . $this->numeroConta . "\n";
-        echo "Saldo: R$ " . $this->saldo . "\n";
+        echo "Nome: ".$this->nome."\n";
+        echo "Sobrenome: ".$this->sobrenome."\n";
+        echo "CPF: ".$this->cpf."\n";
+        echo "Idade: ".$this->idade."\n";
+        echo "Endereço: ".$this->endereco."\n";
+        echo "Número da Conta: ".$this->numeroConta."\n";
+        echo "Saldo: R$ ".$this->saldo."\n";
     }
 }
 
