86ad78ae6d5b4073b2efa851bf814051b979d87c
Maycon Vitor Correa <105678497+MestreWalla@users.noreply.github.com>
2023-11-30 19:53:56 -0300

    Update conectar.php

--- Maycon PHP/SA3PWFE/Criar DB/conectar.php
+++ Maycon PHP/SA3PWFE/Criar DB/conectar.php
@@ -1,13 +1,24 @@
 <?php
 $servername = "127.0.0.1";
-$username = "admin";
-$password = "admin";
-$dbname = "sa3pwfe";
+$username = "root";
+$dbname = "sa3pwfe";  // Adicione esta linha
 
 // Cria a conexão
-$conn = new mysqli($servername, $username, $password, $dbname);
+$conn = new mysqli($servername, $username, '', $dbname);
 
 // Verifica a conexão
 if ($conn->connect_error) {
     die("Conexão falhou: " . $conn->connect_error);
 }
+
+// Cria o banco de dados
+$sql_create_db = "CREATE DATABASE IF NOT EXISTS $dbname";
+if ($conn->query($sql_create_db) === TRUE) {
+    echo "Banco de dados criado com sucesso";
+} else {
+    echo "Erro ao criar banco de dados: " . $conn->error;
+}
+
+// Fecha a conexão
+$conn->close();
+?>
