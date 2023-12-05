96b5b708d0b0c500a8addece5ba3feec30af5c59
Maycon Vitor Correa <105678497+MestreWalla@users.noreply.github.com>
2023-12-05 20:21:49 -0300

    Update processar_edicao.php

--- Maycon PHP/SA3PWFE/processar_edicao.php
+++ Maycon PHP/SA3PWFE/processar_edicao.php
@@ -2,30 +2,41 @@
 session_start();
 include('conectar.php');
 
+// Verificar se o usuário está autenticado
+if (!isset($_SESSION['email'])) {
+    header('Location: login.php');
+    exit();
+}
+
 if ($_SERVER['REQUEST_METHOD'] === 'POST') {
-    // Processar dados do formulário de edição
-    $emailCliente = $_POST['email'];
-    $nome = $_POST['nome'];
-    $sobrenome = $_POST['sobrenome'];
-    // Adicione outros campos conforme necessário
+    // Verificar se as variáveis do formulário estão definidas
+    if (isset($_POST['email'], $_POST['nome'], $_POST['sobrenome'])) {
+        // Processar dados do formulário de edição
+        $emailCliente = $_POST['email'];
+        $nome = $_POST['nome'];
+        $sobrenome = $_POST['sobrenome'];
+        // Adicione outros campos conforme necessário
 
-    // Executar a atualização usando declaração preparada
-    $queryUpdate = "UPDATE clientes SET nome=?, sobrenome=? WHERE email=?";
-    $stmtUpdate = $conexao->prepare($queryUpdate);
+        // Executar a atualização usando declaração preparada
+        $queryUpdate = "UPDATE clientes SET nome=?, sobrenome=? WHERE email=?";
+        $stmtUpdate = $conexao->prepare($queryUpdate);
 
-    if ($stmtUpdate) {
-        $stmtUpdate->bind_param('sss', $nome, $sobrenome, $emailCliente);
-        $stmtUpdate->execute();
+        if ($stmtUpdate) {
+            $stmtUpdate->bind_param('sss', $nome, $sobrenome, $emailCliente);
+            $stmtUpdate->execute();
 
-        if ($stmtUpdate->affected_rows > 0) {
-            $stmtUpdate->close();
-            header('Location: dashboardClientes.php?edit_success=true');
-            exit();
+            if ($stmtUpdate->affected_rows > 0) {
+                $stmtUpdate->close();
+                header('Location: dashboardClientes.php?edit_success=true');
+                exit();
+            } else {
+                echo "Erro ao atualizar o usuário. Erro: " . $stmtUpdate->error;
+            }
         } else {
-            echo "Erro ao atualizar o usuário. Erro: " . $stmtUpdate->error;
+            echo "Erro na preparação da consulta de atualização.";
         }
     } else {
-        echo "Erro na preparação da consulta de atualização.";
+        echo "Parâmetros do formulário ausentes.";
     }
 } else {
     echo "Método de requisição inválido.";
